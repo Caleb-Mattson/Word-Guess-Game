830a6d2298241fca1fe88ea186e5de0c4a088211
CandyMan <c.mattson318@gmail.com>
2019-12-16 14:10:50 -0700

    created tester html to test out theories. loaded up all necessary images in proper folder. Played around with images to attach to randomly selected word -NOT WORKING-

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -8,7 +8,9 @@ var blankSpace = [];
 
 // Make array for words for game
 
-var wordArray = ["stimpy", "johnny"];
+var wordArray = ["bulbasaur", "caterpie", 
+// "charmander", "cubone", "diglett", "eevee", "ekans", "magikarp", "meowth", "mewtwo", "oddish", "onix", "pikachu", "psyduck", "raichu", "snorlax", "squirtle", "zubat"
+];
 
 
         // computer chooses random word.
@@ -17,14 +19,32 @@ var compWord = wordArray[Math.floor(Math.random() * wordArray.length)].toLowerCa
 console.log(compWord);
 
 for (var i = 0; i < compWord.length; i++) {
+    var img = document.createElement(img);
+    
     blankSpace[i] = "_";
     if (compWord === wordArray[0].toLowerCase()) {
-        document.getElementById("hint").innerHTML= "Bestfriend of Ren."
+        img.src = '../images/bulbasaur.png';
+        document.getElementById("imgHint").appendChild(img);
+        document.getElementById("hint").innerHTML= "Bestfriend of Ren.";
     } else if (compWord === wordArray[1].toLowerCase()) {
-        document.getElementById("hint").innerHTML= "Surname Bravo."
+        img.src = '../images/caterpie.png';
+        document.getElementById("imgHint").appendChild(img);
+        document.getElementById("hint").innerHTML= "Surname Bravo.";
     }
 }
 console.log(blankSpace);
 
 document.getElementById("gameboard").innerHTML="" + blankSpace.join(" ");
  
+var imgArray = new Array();
+
+        imgArray[0] = new Image();
+        imgArray[0].src = 'assets/images/bulbasaur.png';
+
+        imgArray[1] = new Image();
+        imgArray[1].src = 'assets/images/caterpie.png';
+
+        /* ... more images ... */
+
+        imgArray[5] = new Image();
+        imgArray[5].src = '';
